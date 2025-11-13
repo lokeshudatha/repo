3bb9ed56401529b5c831ae279f23d07aee1430fa
lokesh udatha <138110408+lokeshudatha@users.noreply.github.com>
2025-11-13 13:53:30 +0530

    Create variables.tf

---
+++ infra/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  type = string
+  default = "winter-monolith-477705-m8"
+}
+variable "region" {
+  type = string
+  default = "central1"
+}
+variable "zone" {
+  type = string
+  default = "central1-a"
+}
+variable "credentials_file" {
+  type = string
+  default = "loki.json"
+}
